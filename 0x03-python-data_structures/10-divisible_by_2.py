b4843b151302c2fc68b184c9f30676e35c5ab5f9
wallacesintra <wallvxwallace962@gmail.com>
2023-12-05 00:15:55 +0300

    divisible by 2

---
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def divisible_by_2(my_list=[]):
+    list_new = []
+    for i in range(len(my_list)):
+        if my_list[i] % 2 == 0:
+            list_new.append(True)
+        else:
+            list_new.append(False)
+    return list_new
