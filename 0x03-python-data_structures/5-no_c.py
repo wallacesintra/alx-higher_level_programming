58797b3926574cf91e5daf64f1de748192470e29
wallacesintra <wallvxwallace962@gmail.com>
2023-12-04 09:20:47 +0300

    remove the Cs

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,4 +1,4 @@
-#!/bin/usr/python3
+#!/usr/bin/python3
 def no_c(my_string):
-    new = my_string.translate({ord(i): None for i in 'Cc'})
-    return new
+    new_string = my_string.translate({ord(i): None for i in 'cC'})
+    return new_string
