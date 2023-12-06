0daf7f54bdd990f7f493b591c3975ebbaa4c530b
wallacesintra <wallvxwallace962@gmail.com>
2023-12-06 21:44:20 +0300

    square list

--- 0x03-python-data_structures/100-print_python_list_info.c
+++ 0x03-python-data_structures/100-print_python_list_info.c
@@ -12,4 +12,4 @@ void print_python_list_info(PyObject *p)
 	printf("[*] Allocated = %li\n", obj->allocated);
 	for (i = 0; i < size; i++)
 		printf("Element %i: %s\n", i, Py_TYPE(obj->ob_item[i])->tp_name);
-}`
+}
