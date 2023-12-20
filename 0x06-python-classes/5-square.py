731282b5d62de583d8a15e0d56939aa7116e2a76
wallacesintra <wallvxwallace962@gmail.com>
2023-12-20 16:34:31 +0300

    visual rep of the area of the square using (#)

---
+++ 0x06-python-classes/5-square.py
@@ -0,0 +1,45 @@
+#!/usr/bin/python3
+"""class of a square"""
+
+
+class Square:
+    """attributes"""
+    def __init__(self, size=0):
+        """instance attributes
+        size: @private size of the square
+        """
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
+
+    @property
+    def size(self):
+        """getter"""
+        return (self.__size)
+
+    @size.setter
+    def size(self, value):
+        """setter"""
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        elif value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
+
+    def area(self):
+        """class method
+        returns the current square area
+        """
+        return int(self.__size) * int(self.__size)
+
+    def my_print(self):
+        """print the square in (#)"""
+        if self.__size == 0:
+            print()
+        else:
+            for i in range(self.__size):
+                for x in range(self.__size):
+                    print("#", end="")
+                print()
