bd8d2c224aa0ad24f8ce674b81839fabf6e4ab0f
wallacesintra <wallvxwallace962@gmail.com>
2023-12-20 15:44:41 +0300

    class method

---
+++ 0x06-python-classes/3-square.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""class of a square"""
+
+
+class Square:
+    """attributes"""
+    def __init__(self, size=0):
+        """instance attributes
+        size: @private size of the square
+        """
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
+
+    def area(self):
+        """class method
+        returns the current square area
+        """
+        return int(self.__size) * int(self.__size)
