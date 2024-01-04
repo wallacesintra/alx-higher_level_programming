b966d88b6eb5b5bdaef870085bd0869b98d1dc9f
wallacesintra <wallvxwallace962@gmail.com>
2024-01-04 17:57:03 +0300

    print area representation

---
+++ 0x08-python-more_classes/3-rectangle.py
@@ -0,0 +1,74 @@
+#!/usr/bin/python3
+
+
+"""
+rectangle class
+"""
+
+
+class Rectangle():
+    """Rectangle class"""
+    def __init__(self, width=0, height=0):
+        """private attributes width and height"""
+        if not isinstance(width, int):
+            raise TypeError("width must be an integer")
+        if width < 0:
+            raise ValueError("width must be >= 0")
+        if not isinstance(height, int):
+            raise TypeError("height must be an integer")
+        if height < 0:
+            raise ValueError("height must be >= 0")
+
+        self.__width = width
+        self.__height = height
+
+    @property
+    def width(self):
+        """get width"""
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        """set width"""
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = value
+
+    @property
+    def height(self):
+        """get height"""
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        """set height"""
+        if not isinstance(value, int):
+            raise TypeError("height must be an integer")
+        if value < 0:
+            raise ValueError("height must be >= 0")
+        self.__height = value
+
+    def area(self):
+        """returns the rectangle area"""
+        return int(self.__height) * int(self.__width)
+
+    def perimeter(self):
+        """returns the rectangle perimeter"""
+        if self.__height == 0 or self.__width == 0:
+            return 0
+        return (int(self.__height) + int(self.__width)) * 2
+
+    def __str__(self) -> str:
+        """print the rectangle with the character #"""
+        if self.__height == 0 or self.__width == 0:
+            return ""
+
+        area = ""
+        for i in range(self.__height):
+            for x in range(self.__width):
+                area += "#"
+            if i < self.__height - 1:
+                area += "\n"
+        return (area)
