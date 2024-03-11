9614a69035aa035037522d996b77e33e8ccb1c0f
wallacesintra <wallvxwallace962@gmail.com>
2024-03-11 23:05:57 +0300

    loops

---
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+const i = parseInt(process.argv[2]);
+
+if (isNaN(i)) {
+  console.log('Missing number of occurrences');
+} else {
+  for (let j = 0; j < i; j++) {
+    console.log('C is fun');
+  }
+}
