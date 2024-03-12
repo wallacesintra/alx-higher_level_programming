6636eecba8f756ff05fbedfdac05255d0db43eab
wallacesintra <wallvxwallace962@gmail.com>
2024-03-12 22:44:50 +0300

    class methods

---
+++ 0x13-javascript_objects_scopes_closures/3-rectangle.js
@@ -0,0 +1,29 @@
+#!/usr/bin/node
+/**
+ * class Rectangle that defines a rectangle
+ * If w or h is equal to 0 or not a positive integer, create an empty object
+*/
+
+class Rectangle {
+  constructor (w, h) {
+    if (w > 0 && h > 0) {
+      this.width = w;
+      this.height = h;
+    }
+  }
+
+    print () {
+        let i;
+        let x;
+        let str = '';
+        for (i = 0; i < this.height; i++) {
+        for (x = 0; x < this.width; x++) {
+            str += 'X';
+        }
+        console.log(str);
+        str = '';
+        }
+    }
+}
+
+module.exports = Rectangle;
