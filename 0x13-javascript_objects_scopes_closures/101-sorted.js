76a3ac7b5ecd9fb7cda8c5c2484b8a8ba9f50d2b
wallacesintra <wallvxwallace962@gmail.com>
2024-03-13 00:33:33 +0300

    dictionary

---
+++ 0x13-javascript_objects_scopes_closures/101-sorted.js
@@ -0,0 +1,13 @@
+#!/usr/bin/node
+/**
+ *imports a dictionary of occurrences by user id and computes a dictionary of user ids by occurrence.
+ */
+const dict = require('./101-data.js').dict;
+const newDict = {};
+for (const key in dict) {
+  if (!newDict[dict[key]]) {
+    newDict[dict[key]] = [];
+  }
+  newDict[dict[key]].push(key);
+}
+console.log(newDict);
