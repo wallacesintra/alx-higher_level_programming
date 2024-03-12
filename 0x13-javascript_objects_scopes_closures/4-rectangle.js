872fba428b2cecbbe44d2f1a8bff610703b3e54b
wallacesintra <wallvxwallace962@gmail.com>
2024-03-12 23:07:53 +0300

    Fix print method and add comments to Rectangle class

--- 0x13-javascript_objects_scopes_closures/4-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/4-rectangle.js
@@ -21,16 +21,16 @@ class Rectangle {
     }
   }
 
-    rotate () {
-        const temp = this.width;
-        this.width = this.height;
-        this.height = temp;
-    }
+  rotate () {
+    const temp = this.width;
+    this.width = this.height;
+    this.height = temp;
+  }
 
-    double () {
-        this.width *= 2;
-        this.height *= 2;
-    }
+  double () {
+    this.width *= 2;
+    this.height *= 2;
+  }
 }
 
 module.exports = Rectangle;
