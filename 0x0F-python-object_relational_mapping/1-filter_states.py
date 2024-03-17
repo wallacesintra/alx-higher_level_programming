6228be91b6cdb06b1f1bef18222f204cf58ce0ab
wallacesintra <wallvxwallace962@gmail.com>
2024-03-17 18:48:20 +0300

    filter db

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -12,7 +12,8 @@ if __name__ == "__main__":
         host="localhost",
         user=sys.argv[1],
         passwd=sys.argv[2],
-        db=sys.argv[3]
+        db=sys.argv[3],
+        charset="utf8"
     )
     cursor = db.cursor()
     cursor.execute("SELECT * FROM states WHERE name LIKE 'N%' ORDER BY id ASC")
