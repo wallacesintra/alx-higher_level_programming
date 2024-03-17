f083ecc77477e19924fe6b6939bfe53c3337264a
wallacesintra <wallvxwallace962@gmail.com>
2024-03-18 01:07:07 +0300

    Refactor code to use command line arguments and filter states starting with 'N'

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -5,16 +5,16 @@ from the database hbtn_0e_0_usa
 """
 
 import MySQLdb
-import sys
+from sys import argv
 
 if __name__ == "__main__":
-    db = MySQLdb.connect(
-        host="localhost", user=sys.argv[1], passwd=sys.argv[2],
-        db=sys.argv[3], charset="utf8"
-    )
+    db = MySQLdb.connect(host="localhost", port=3306, user=argv[1],
+                         passwd=argv[2], db=argv[3], charset="utf8")
     cursor = db.cursor()
     cursor.execute("SELECT * FROM states WHERE name LIKE 'N%' ORDER BY id ASC")
-    for row in cursor.fetchall():
-        print(row)
+    rows = cursor.fetchall()
+    for row in rows:
+        if row[1][0] == 'N':
+            print(row)
     cursor.close()
     db.close()
