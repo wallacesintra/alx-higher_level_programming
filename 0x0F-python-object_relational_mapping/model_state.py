368a954322ed233990b0613a4296f8d68034f3d2
wallacesintra <wallvxwallace962@gmail.com>
2024-03-17 23:57:35 +0300

    Update State class docstring

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -10,7 +10,7 @@ from sqlalchemy.ext.declarative import declarative_base
 Base = declarative_base()
 
 class State(Base):
-    """Class representing the states table"""
+    """representing the states table"""
     __tablename__ = 'states'
     id = Column(Integer, primary_key=True, autoincrement=True, nullable=False)
     name = Column(String(256), nullable=False)
