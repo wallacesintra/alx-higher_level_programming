205e7c35d755a793ef446967be486a168b5660f4
wallacesintra <wallvxwallace962@gmail.com>
2024-03-18 01:05:50 +0300

    Refactor MySQLdb connection parameters in 1-filter_states.py

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -9,11 +9,8 @@ import sys
 
 if __name__ == "__main__":
     db = MySQLdb.connect(
-        host="localhost",
-        user=sys.argv[1],
-        passwd=sys.argv[2],
-        db=sys.argv[3],
-        charset="utf8"
+        host="localhost", user=sys.argv[1], passwd=sys.argv[2],
+        db=sys.argv[3], charset="utf8"
     )
     cursor = db.cursor()
     cursor.execute("SELECT * FROM states WHERE name LIKE 'N%' ORDER BY id ASC")
