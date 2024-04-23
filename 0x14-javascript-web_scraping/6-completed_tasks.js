c2cc97e776b77b7f052d310811c5d25d952720af
wallacesintra <wallvxwallace962@gmail.com>
2024-04-23 23:57:12 +0300

    Refactor completed tasks script for improved readability and variable naming consistency

--- 0x14-javascript-web_scraping/6-completed_tasks.js
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -8,21 +8,21 @@ const req = require('request');
 const url = process.argv[2];
 
 req(url, (error, response, body) => {
-    if (error) {
-        console.log(error);
-    } else {
-        const tasks = JSON.parse(body);
-        const completed_tasks = tasks.filter((task) => task.completed === true);
-        // console.log(completed_tasks.length);
+  if (error) {
+    console.log(error);
+  } else {
+    const tasks = JSON.parse(body);
+    const completedTasks = tasks.filter((task) => task.completed === true);
+    // console.log(completed_tasks.length);
 
-        const completed_tasks_by_user = {};
-        for (const task of completed_tasks) {
-            if (task.userId in completed_tasks_by_user) {
-                completed_tasks_by_user[task.userId]++;
-            } else {
-                completed_tasks_by_user[task.userId] = 1;
-            }
-        }
-        console.log(completed_tasks_by_user);
+    const completedTaskByUser = {};
+    for (const task of completedTasks) {
+      if (task.userId in completedTaskByUser) {
+        completedTaskByUser[task.userId]++;
+      } else {
+        completedTaskByUser[task.userId] = 1;
+      }
     }
+    console.log(completedTaskByUser);
+  }
 });
