3d881ffe549b275dfa898a743d3c528366b3882b
wallacesintra <wallvxwallace962@gmail.com>
2024-04-23 22:51:55 +0300

    request module

---
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -0,0 +1,25 @@
+#!/usr/bin/node
+
+/**
+ * script prints the number of movies where the character “Wedge Antilles” is present
+ */
+
+const req = require('request');
+const url = process.argv[2];
+
+req(url, (error, response, body) => {
+    if (error) {
+        console.log(error);
+    } else {
+        const films = JSON.parse(body).results;
+        let i = 0;
+        for (const film of films) {
+        for (const character of film.characters) {
+            if (character.includes('18')) {
+            i++;
+            }
+        }
+        }
+        console.log(i);
+    }
+});
