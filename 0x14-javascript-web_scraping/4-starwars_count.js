d603c54504ef517d3d14b5137f15046851ee44a7
wallacesintra <wallvxwallace962@gmail.com>
2024-04-23 22:55:51 +0300

    Refactor starwars_count.js to use arrow functions and improve readability

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -8,18 +8,18 @@ const req = require('request');
 const url = process.argv[2];
 
 req(url, (error, response, body) => {
-    if (error) {
-        console.log(error);
-    } else {
-        const films = JSON.parse(body).results;
-        let i = 0;
-        for (const film of films) {
-        for (const character of film.characters) {
-            if (character.includes('18')) {
-            i++;
-            }
+  if (error) {
+    console.log(error);
+  } else {
+    const films = JSON.parse(body).results;
+    let i = 0;
+    for (const film of films) {
+      for (const character of film.characters) {
+        if (character.includes('18')) {
+          i++;
         }
-        }
-        console.log(i);
+      }
     }
+    console.log(i);
+  }
 });
