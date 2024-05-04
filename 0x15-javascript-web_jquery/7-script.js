b6136324f3c4319e28024e834d462e29117d3b66
wallacesintra <wallvxwallace962@gmail.com>
2024-05-04 23:14:12 +0300

    get character from api

---
+++ 0x15-javascript-web_jquery/7-script.js
@@ -0,0 +1,13 @@
+#!/usr/bin/node
+
+/** 
+ * fetches the character name from this URL: https://swapi-api.alx-tools.com/api/people/5/?format=json
+ * 
+ * The name must be displayed in the HTML tag DIV#character
+ * 
+*/
+
+$.get('https://swapi-api.alx-tools.com/api/people/5/?format=json', function (data) {
+  $('DIV#character').text(data.name);
+}
+);
