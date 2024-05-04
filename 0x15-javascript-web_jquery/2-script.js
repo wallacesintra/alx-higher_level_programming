42587d6019a4d1707bf1f7feb2b7afed0b5c8309
wallacesintra <wallvxwallace962@gmail.com>
2024-05-04 22:55:46 +0300

    onclick function

---
+++ 0x15-javascript-web_jquery/2-script.js
@@ -0,0 +1,13 @@
+#!/usr/bin/node
+
+/**
+ * updates the text color of the <header> element to red
+ * (#FF0000) when the user clicks on the tag DIV#red_header
+ *
+ * can’t use document.querySelector to select the HTML tag
+ */
+
+$('DIV#red_header').click(function () {
+  $('header').css('color', '#FF0000');
+}
+);
