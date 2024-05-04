968dc6b3ed490060d70d13fe21d666293ccbc36e
wallacesintra <wallvxwallace962@gmail.com>
2024-05-04 23:44:29 +0300

    get text

--- 0x15-javascript-web_jquery/9-script.js
+++ 0x15-javascript-web_jquery/9-script.js
@@ -3,7 +3,7 @@
 /**
  * fetches from https://hellosalut.stefanbohacek.dev/?lang=fr and displays the value of hello from that fetch in the HTML tag DIV#hello
  * The translation of “hello” must be displayed in the HTML tag DIV#hello
- * 
+ *
  */
 
 $.get('https://fourtonfish.com/hellosalut/?lang=fr', function (data) {
